68e3221ef3a71ca998f1b57ab31fec15e2cdc6a4
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-23 17:42:07 +0500

    SQL Server 2025

--- CDE-SQL Server/Left Join.sql
+++ CDE-SQL Server/Left Join.sql
@@ -1,7 +1,10 @@
 select * from hr.candidates;
 select * from hr.employees;
 
-
 select * from hr.employees e left join hr.candidates c on e.fullname = c.fullname;
 
 select * from hr.employees e right join hr.candidates c on e.fullname = c.fullname;
+
+select p.product_name, ot.order_id, p.list_price from production.products p left join  sales.order_items ot on ot.product_id = p.product_id order by ot.order_id;
+
+select p.product_name, ot.order_id, ot.item_id, o.order_date from production.products p left join  sales.order_items ot on ot.product_id = p.product_id left join sales.orders o on o.order_id = ot.order_id order by ot.order_id;
