90539e12041db3507229684c9a60055b08c5ace9
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-26 17:24:33 +0500

    SQL Server 2025

--- CDE-SQL Server/Group By Clause.sql
+++ CDE-SQL Server/Group By Clause.sql
@@ -52,3 +52,33 @@ group by
 order by
     c.city;
 
+
+-- Show the total quantity of each product ordered across all orders. 
+-- Display the product name and the total quantity ordered.
+
+select 
+    p.product_name, sum(ot.quantity) total_product_ordered
+from
+    sales.order_items ot
+join
+    production.products p 
+    on p.product_id = ot.product_id
+group by 
+    p.product_name;
+
+-- List all customers and the total number of orders they have placed. 
+-- Show only those customers who have placed more than 2 orders.
+
+select concat(first_name, ' ', last_name) customer, count(o.order_id) ordered_placed from sales.customers c inner join sales.orders o on c.customer_id = o.customer_id group by c.first_name, c.last_name having count(o.order_id) > 2; 
+
+select 
+    concat(c.first_name, ' ', c.last_name) as customer,
+    count(o.order_id) as orders_placed
+from 
+    sales.customers c
+inner join 
+    sales.orders o on c.customer_id = o.customer_id
+group by 
+    c.first_name, c.last_name
+having 
+    count(o.order_id) > 2;
