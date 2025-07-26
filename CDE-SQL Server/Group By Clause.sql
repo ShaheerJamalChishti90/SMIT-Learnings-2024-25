eca02ac29527eb1fdb446378176732af7f15ecde
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-07-26 16:24:38 +0500

    SQL Server 2025

--- CDE-SQL Server/Group By Clause.sql
+++ CDE-SQL Server/Group By Clause.sql
@@ -26,3 +26,29 @@ order by
     c.city,
     c.state;
 
+
+select c.state, count(c.customer_id) total_cities from sales.customers c group by c.state;
+
+
+select o.customer_id, year(o.order_date) from sales.orders o where o.customer_id in (1, 2) order by o.customer_id;
+
+select 
+    o.customer_id, year(o.order_date) order_year, count(o.order_id) order_placed
+from 
+    sales.orders o 
+where 
+    year(o.order_date) > 2017
+group by 
+    o.customer_id, year(o.order_date) 
+order by 
+    o.customer_id;
+
+select 
+    c.city, count(c.customer_id) customer_count
+from 
+    sales.customers c
+group by
+    c.city
+order by
+    c.city;
+
