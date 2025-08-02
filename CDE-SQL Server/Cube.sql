1de82f395d339f25a03134943759763f7f639256
ShaheerJamalChishti90 <shaheerjamal369@gmail.com>
2025-08-02 15:10:36 +0500

    SQL Server 2025

---
+++ CDE-SQL Server/Cube.sql
@@ -0,0 +1,23 @@
+select 
+	pb.brand_name, pc.category_name, pp.model_year, sum(ot.list_price * ot.quantity * (1 - ot.discount)) as sales INTO sales.sales_summary
+from 
+	production.products pp 
+inner join 
+	production.categories pc 
+	on pp.category_id = pc.category_id 
+inner join 
+	production.brands pb 
+	on pp.brand_id = pb.brand_id 
+inner join 
+	sales.order_items ot  
+	on ot.product_id = pp.product_id 
+group by 
+	pb.brand_name, pc.category_name, pp.model_year 
+order by 
+	pb.brand_name;
+
+-- Cube Clause
+select brand_name, category_name, sum(sales) as total_sales from sales.sales_summary group by  cube(brand_name, category_name);
+
+-- Partial Grouping
+select brand_name, category_name, sum(sales) as total_sales from sales.sales_summary group by  brand_name, cube (category_name);
